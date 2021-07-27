545dae17c95790a0e4500216899db95a50d24786
Rachel Myers <rachelmyers@google.com>
2021-07-27 12:00:53 -0700

    Remove unused functions code

--- final-state/functions/index.js
+++ final-state/functions/index.js
@@ -12,30 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const firestore = admin.initializeApp().firestore();
 
-// Publishes draft posts. Triggered via a button click by admin user.
+// Placeholder function
 exports.publishPost = functions.https.onRequest((req, res) => {
-  // Fetch post from `drafts`
-  const body = JSON.parse(req.body);
-  let post = firestore.document(`drafts/${body.draftID}`);
-
-  // Modify post's fields
-  post.publishedAt = firestore.Timestamp.fromDate(new Date());
-  delete post.createdAt;
-  post.visible = true;
-
-  // Create new updated post
-  firestore.document(`published/${body.draftID}`).create(post)
-    .then(() => res.json({status: 200}));
-});
-
-// Marks published drafts as hidden.
-exports.softDelete = functions.https.onRequest((req, res) => {
-  const body = JSON.parse(req.body);
-  // Flip `visible` field to false;
-  firestore.document(`published/${body.postID}`).update({visible: false})
-    .then(() => res.json({status: 200}));
+  res.json({status: 200});
 });
